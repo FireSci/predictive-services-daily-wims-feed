56425f48a2aba563ce11e6b116067a0b5514b9d2
jcla490 <jclark754@gmail.com>
2022-05-15 14:22:32 -0400

    write headers, labels

--- handler.py
+++ handler.py
@@ -1,49 +1,120 @@
 import json
 import os
 import logging
-from datetime import date
+from re import S
+import typing as T
+from datetime import datetime
 
 import boto3
+import requests
+import xmltodict
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-# Required WIMS params
-#   stn=??
-#   start=dd-mon-yy
-#   end=dd-mon-yy
-#   ndays=??
-#   * note leaving start and end blank will return most recent x days
-
 OUTPUT_PATH = "ndfd_predserv_fcst.txt"
 
 
-def get_station_data(stn_id: str):
-    pass
+def get_station_data(urls: T.Dict[str, str]) -> T.Dict[str, T.Dict]:
+    """Given a list of URLs for a particular station, returns data from WIMS"""
+    stn_data: T.Dict = {}
+    for url in urls.values():
+        response = requests.get(url)
+        dict_data = xmltodict.parse(response.content)
+        # Since we call nfdrs endpoint twice
+        if "nfdrs" in stn_data and "nfdrs" in dict_data:
+            dict_data["nfdrs_retro"] = dict_data.pop("nfdrs")
+        stn_data = {**stn_data, **dict_data}
+    return stn_data
+
+
+def get_stn_headers(stn_data) -> T.List[str]:
+    """Helper func to extract headers from first day of any endpoint result"""
+    for k in stn_data:
+        try:
+            s = stn_data["nfdrs"]["row"][0]
+            today_dt = datetime.utcnow()
+            headers = [
+                f"*{s['sta_nm']}",
+                s["sta_id"],
+                s["latitude"],
+                s["longitude"],
+                today_dt.strftime("%Y%m%d"),
+                today_dt.strftime("%H"),
+            ]
+            break
+        # This just makes sure we write headers from at least 1 endpoint
+        except KeyError:
+            continue
+    return headers
+
+
+def process_data(stn_data):
+    out_stn = {}
+
+    # Get headers (stn name, stn id, etc.)
+    out_stn["headers"] = get_stn_headers(stn_data)
+
+    try:
+        pass
+    except Exception as e:
+        logger.info(e)
+    return out_stn
 
 
 def run(event, context):
+    """Main lambda handler"""
     S3 = boto3.client("s3")
+    start_date: str = datetime.utcnow().strftime("%d-%b-%y")
 
-    # start_date = date.today().strftime("%d-%b-%y")
+    # Load station data into memory
+    with open("station_list.json") as json_file:
+        stns: T.List[T.Dict[str, T.Any]] = json.load(json_file)
 
-    # with open("station_list.json") as json_file:
-    #     stn_ids = json.load(json_file)["stations"]
+    for stn in stns:
+        # Build request urls
+        urls = {
+            # Gets next seven day NFDRS16 forecast
+            "nfdrs_forecast": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn['STNID']}&type=F&priority=&fmodel=16Y&ndays=7",
+            # Gets past and Day 0 NFDRS obs
+            "nfdrs_retro": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn['STNID']}&type=N&priority=&fmodel=16Y&ndays=1",
+            # Gets next seven day wx forecast
+            "wx_forecast": f"https://famprod.nwcg.gov/wims/xsql/pfcst.xsql?stn={stn['STNID']}&type=F&start={start_date}&ndays=7",
+            # Gets past and Day 0 wx obs
+            "wx_retro": f"https://famprod.nwcg.gov/wims/xsql/obs.xsql?stn={stn['STNID']}&ndays=1",
+        }
 
-    # for stn_id in stn_ids:
-    #     # Note, sorting applies to wx_retro,
-    #     urls = {
-    #         "nfdrs_forecast": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn_id}&type=F&priority=&fmodel=16Y&sort=asc&ndays=7",
-    #         "nfdrs_retro": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn_id}&type=N&priority=&fmodel=16Y&ndays=1&sort=asc",
-    #         "wx_forecast": f"https://famprod.nwcg.gov/wims/xsql/pfcst.xsql?stn={stn_id}&type=F&start={start_date}&ndays=7&sort=asc",
-    #         "wx_retro": f"https://famprod.nwcg.gov/wims/xsql/obs.xsql?stn={stn_id}&type=O&ndays=1&sort=asc",
-    #     }
+        # Make requests
+        stn_data = get_station_data(urls)
 
-    #     break
+        # Process only the data we need
+        stn_data = process_data(stn_data)
 
-    # Get all data for stations
+        # Write labels, this feels hacky
+        stn_data["labels"] = {
+            "Fcst Dy": [
+                "Max RH (%)",
+                "Min Temp (F)",
+                "Min RH (%)",
+                "Max Temp (F)",
+                "WSpd (knt)",
+                "BI",
+                "ERC",
+                "IC",
+                "10-hr fuel (%)",
+                "100-hr fuel (%)",
+                "1000-hr fuel (%)",
+            ]
+        }
+        print(stn_data)
 
-    # Format station data
+        #    {
+        #         "STNID": 20207,
+        #         "RS": 13,
+        #         "MP": 2,
+        #         "MSGC": "16Y1P"
+        #     },
+        # Format station data
 
     # Write to txt file with tabs and new lines where needed
     with open(OUTPUT_PATH, "w") as f:
--- parse_stations/parse_stn_ids.py
+++ parse_stations/parse_stn_ids.py
@@ -7,18 +7,19 @@ import json
 import pandas as pd
 
 # Drop GACC and PSA columns
-df = pd.read_csv("PSA_national_station_list_04-15-22.csv").drop(
-    ["GACC", "PSA"], axis=1
+df = pd.read_csv("SWA_stnlist_robust_05-14-22.csv").drop(
+    ["NAME", "AGENCY", "PSA", "SHEFID"], axis=1
 )
 
-# Get only the unique values from all remaining columns. Ravel tells pandas to
-# flatten the columns into one series
-df = pd.unique(df[df.columns].values.ravel("k"))
+# This was for national file
+# # Get only the unique values from all remaining columns. Ravel tells pandas to
+# # flatten the columns into one series
+# df = pd.unique(df[df.columns].values.ravel("k"))
 
-# Remove nan if it gets picked up from file, convert to ints to remove trailing
-# zeroes, then to string for easier use later in JSON
-df = df[~pd.isnull(df)].astype(int).astype(str)
+# # Remove nan if it gets picked up from file, convert to ints to remove trailing
+# # zeroes, then to string for easier use later in JSON
+# df = df[~pd.isnull(df)].astype(int).astype(str)
 
 # Dump to JSON, sorted for fun
 with open("../station_list.json", "w") as outfile:
-    json.dump({"stations": sorted(list(df))}, outfile)
+    json.dump(df.to_dict("records"), outfile)
