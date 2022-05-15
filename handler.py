a914c9102a105cc34b4bd3dcd01820709a933977
jcla490 <jclark754@gmail.com>
2022-05-15 11:49:02 -0400

    plumb in file upload

--- handler.py
+++ handler.py
@@ -1,7 +1,9 @@
 import json
+import os
 import logging
 from datetime import date
 
+import boto3
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -13,24 +15,46 @@ logger.setLevel(logging.INFO)
 #   ndays=??
 #   * note leaving start and end blank will return most recent x days
 
+OUTPUT_PATH = "ndfd_predserv_fcst.txt"
+
 
 def get_station_data(stn_id: str):
     pass
 
 
 def run(event, context):
-    start_date = date.today().strftime("%d-%b-%y")
+    S3 = boto3.client("s3")
 
-    with open("station_list.json") as json_file:
-        stn_ids = json.load(json_file)["stations"]
+    # start_date = date.today().strftime("%d-%b-%y")
 
-    for stn_id in stn_ids:
-        # Note, sorting applies to wx_retro,
-        urls = {
-            "nfdrs_forecast": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn_id}&type=F&priority=&fmodel=16Y&sort=asc&ndays=7",
-            "nfdrs_retro": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn_id}&type=N&priority=&fmodel=16Y&ndays=1&sort=asc",
-            "wx_forecast": f"https://famprod.nwcg.gov/wims/xsql/pfcst.xsql?stn={stn_id}&type=F&start={start_date}&ndays=7&sort=asc",
-            "wx_retro": f"https://famprod.nwcg.gov/wims/xsql/obs.xsql?stn={stn_id}&type=O&ndays=1&sort=asc",
-        }
+    # with open("station_list.json") as json_file:
+    #     stn_ids = json.load(json_file)["stations"]
 
-        break
+    # for stn_id in stn_ids:
+    #     # Note, sorting applies to wx_retro,
+    #     urls = {
+    #         "nfdrs_forecast": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn_id}&type=F&priority=&fmodel=16Y&sort=asc&ndays=7",
+    #         "nfdrs_retro": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn_id}&type=N&priority=&fmodel=16Y&ndays=1&sort=asc",
+    #         "wx_forecast": f"https://famprod.nwcg.gov/wims/xsql/pfcst.xsql?stn={stn_id}&type=F&start={start_date}&ndays=7&sort=asc",
+    #         "wx_retro": f"https://famprod.nwcg.gov/wims/xsql/obs.xsql?stn={stn_id}&type=O&ndays=1&sort=asc",
+    #     }
+
+    #     break
+
+    # Get all data for stations
+
+    # Format station data
+
+    # Write to txt file with tabs and new lines where needed
+    with open(OUTPUT_PATH, "w") as f:
+        f.write("test" + "\t" + "with tabs")
+
+    # Save to S3, remove local file
+    with open(OUTPUT_PATH, "rb") as f:
+        S3.upload_fileobj(
+            f,
+            "predictive-services-open-data-us-west-2",
+            OUTPUT_PATH,
+        )
+    os.remove(OUTPUT_PATH)
+    return {"message": "WIMS data successfully written"}
