ed7aef954d7054d87ca0da32e88cdbd64cdf49a0
jcla490 <jclark754@gmail.com>
2022-05-16 20:46:57 -0400

    add wims obs/fcst

--- handler.py
+++ handler.py
@@ -1,7 +1,6 @@
 import json
 import os
 import logging
-from re import S
 import typing as T
 from datetime import datetime
 
@@ -15,11 +14,11 @@ logger.setLevel(logging.INFO)
 OUTPUT_PATH = "ndfd_predserv_fcst.txt"
 
 
-def get_station_data(urls: T.Dict[str, str]) -> T.Dict[str, T.Dict]:
+def get_station_data(urls: T.List[str]) -> T.Dict[str, T.Dict]:
     """Given a list of URLs for a particular station, returns data from WIMS.
     This should also return values for all endpoints even if its just None"""
     stn_data: T.Dict = {}
-    for url in urls.values():
+    for url in urls:
         response = requests.get(url)
         dict_data = xmltodict.parse(response.content)
         # Since we call nfdrs endpoint twice, this works because of order of urls
@@ -53,15 +52,50 @@ def get_stn_headers(stn_data) -> T.List[str]:
     return headers
 
 
-def process_data(stn_data):
+def remove_non_rs_obs(obs: T.List[T.Dict], rs: int) -> T.List[T.Dict]:
+    """Itty bitty helper for removing obs that don't match our regularly
+    scheduled time"""
+    return [ob for ob in obs if ob["obs_tm"] == str(rs)]
+
+
+def process_data(stn_data: T.Dict[str, T.Dict], rs: int, mp: int, msgc: str):
     """Process all of the required data for a station"""
     out_stn = {}
 
-    # Get headers (stn name, stn id, etc.)
+    # Station headers (stn name, stn id, etc.)
     out_stn["headers"] = get_stn_headers(stn_data)
 
-    # Weather forecast data
+    ###########################################################################
+    #  Weather forecast processing                                            #
+    ###########################################################################
     pfcst: T.List[T.Dict] = stn_data["pfcst"]["row"]
+    # TODO: this needs to only ever be 7
+    assert len(pfcst) == 7
+    for day in pfcst:
+        # Remove year from date
+        out_stn[day["fcst_dt"][:-5]] = [
+            day["rh_max"],
+            day["temp_min"],
+            day["rh_min"],
+            day["temp_max"],
+            day["wind_sp"],
+        ]
+
+    ###########################################################################
+    # Weather obs processing                                                  #
+    ###########################################################################
+    obs: T.List[T.Dict] = stn_data["obs"]["row"]
+    obs = remove_non_rs_obs(obs, rs)
+    # TODO: this needs to only ever be 2
+    assert len(obs) == 2
+    for day in obs:
+        out_stn[day["obs_dt"][:-5]] = [
+            day["rh_max"],
+            day["temp_min"],
+            day["rh_min"],
+            day["temp_max"],
+            day["wind_sp"],
+        ]
 
     return out_stn
 
@@ -79,22 +113,22 @@ def run(event, context):
 
     for stn in stns:
         # Build request urls
-        urls = {
+        urls = [
             # Gets next seven day NFDRS16 forecast
-            "nfdrs_forecast": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn['STNID']}&type=F&priority=&fmodel=16Y&ndays=7",
+            f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn['STNID']}&type=F&priority=&fmodel=16Y&ndays=7",
             # Gets past and Day 0 NFDRS obs
-            "nfdrs_retro": f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn['STNID']}&type=N&priority=&fmodel=16Y&ndays=1",
+            f"https://famprod.nwcg.gov/wims/xsql/nfdrs.xsql?stn={stn['STNID']}&type=N&priority=&fmodel=16Y&ndays=1",
             # Gets next seven day wx forecast
-            "wx_forecast": f"https://famprod.nwcg.gov/wims/xsql/pfcst.xsql?stn={stn['STNID']}&type=F&start={start_date}&ndays=7",
+            f"https://famprod.nwcg.gov/wims/xsql/pfcst.xsql?stn={stn['STNID']}&type=F&start={start_date}&ndays=7",
             # Gets past and Day 0 wx obs
-            "wx_retro": f"https://famprod.nwcg.gov/wims/xsql/obs.xsql?stn={stn['STNID']}&ndays=1",
-        }
+            f"https://famprod.nwcg.gov/wims/xsql/obs.xsql?stn={stn['STNID']}&ndays=1",
+        ]
 
-        # Make requests
+        # Make requests to WIMS endpoints
         stn_data = get_station_data(urls)
 
         # Checks if one endpoint failed. If so, log it and try next station
-        # We enforce that a station has data for all urls
+        # TODO: We enforce that a station has data for all urls
         if None in stn_data.values():
             error_stns.append(stn["STNID"])
             logger.info(
@@ -103,7 +137,7 @@ def run(event, context):
             continue
 
         # Process only the data we need
-        stn_data = process_data(stn_data)
+        stn_data = process_data(stn_data, stn["RS"], stn["MP"], stn["MSGC"])
 
         # Write labels
         stn_data["labels"] = {
